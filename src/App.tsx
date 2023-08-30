c836957ec220a05a229d0fa1c1d521d4f5cd93de
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-08-30 18:37:09 +0200

    setup routes structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom"
 import "./index.css"
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold"> Note Nest 🪹 </h1>
+    <div className="py-8 px-8">
+    <Routes> 
+      <Route path="/" element={<h1 className="text-3xl font-bold"> Note Nest 🪹 </h1>} />
+      <Route path="/new" element={<h1> New </h1>} />
+      <Route path="/:id">
+        <Route index element={<h1> Show </h1>} />
+        <Route path="edit" element={<h1> Edit </h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+    </div>
   )
 }
 
